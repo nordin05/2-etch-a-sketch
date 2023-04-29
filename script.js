4c0d3951906b7c1984ec63ba8b6543758170f94e
Nordin <128930417+nordin05@users.noreply.github.com>
2023-04-29 15:12:41 +0200

    Updated createGrid function so that it can be dynamically updated

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-let gridSize = 8;
+let gridSize = 640; 
+let gridRows = 8;
+let cellWidth = gridSize / gridRows;
 let mode;
 
 function createGrids(){
     let grid = document.querySelector('.container');
 
-    for (i = 0; i < gridSize; i++){
+    if (grid.hasChildNodes()) {
+        grid.replaceChildren();
+    }
+
+    for (i = 0; i < gridRows; i++){
         row = document.createElement("div");
         row.id = "row" + (i+1);
         grid.appendChild(row);
 
         rowW = document.querySelector('#row' + (i+1));
         console.log(rowW);
-        for(j = 0; j < gridSize; j++){
+        for(j = 0; j < gridRows; j++){
             cell = document.createElement("div");
             cell.classList.add('cell', (j+1));
+            cell.style.width = (cellWidth + 'px');
+            cell.style.height = (cellWidth + 'px');
             rowW.appendChild(cell);
             
         }
     }
+
+    Cells = document.querySelectorAll(".cell");
+    Cells.forEach((Cell) => { drawOnCell('click', Cell) });
 }
 
 function drawOnCell(mode, Cell){
@@ -35,7 +46,9 @@ function drawOnCell(mode, Cell){
 }
 
 createGrids();
-Cells = document.querySelectorAll(".cell");
-Cells.forEach((Cell) => {
-    drawOnCell('click', Cell)
-});
+
+gridRows = 50;
+cellWidth = gridSize / gridRows;
+setTimeout(function() { 
+    createGrids();
+}, 1000);
